bf39173bf9a5d99787dc5ea525a708da35ffe1e4
Adam Žák <adam.zak@softpoint.sk>
2022-09-25 00:36:02 +0200

    Add first import of authors (main)

--- create_tables.rb
+++ create_tables.rb
@@ -1,16 +1,15 @@
-puts 'MORE'
-
-def create_authors
-=begin
-  DB.create_table :items do
-    primary_key :id, type: :Bignum
-    String :name
-    String :username
-    String :description, text: true
-    Integer :followers_count
-    Integer :following_count
-    Integer :tweet_count
-    Integer :listed_count
+def create_tables(database)
+  unless database.table_exists?(:authors)
+    database.create_table :authors do
+       primary_key :id, type: :Bignum
+       String :name
+       String :username
+       String :description, text: true
+       Integer :followers_count
+       Integer :following_count
+       Integer :tweet_count
+       Integer :listed_count
+     end
   end
-=end
 end
+puts 'Zrobena tabulecka ;)'
--- import.rb
+++ import.rb
@@ -1,6 +1,7 @@
 require 'sequel'
 require 'dotenv'
 load 'create_tables.rb'
+load 'create_author_files.rb'
 
 Dotenv.load
 
@@ -12,5 +13,36 @@ DATABASE = Sequel.connect(adapter: :postgres,
                     database: ENV.fetch("POSTGRES_DB"),
                     max_connections: 10)
                     #logger: Logger.new('log/db.log'))
+
+create_tables(DATABASE)
+                    #create_smaller_author_files
+
+                    #=begin
+
+i = 0
+array_of_authors = []
+File.open("/home/adam/Downloads/autori_2.jsonl", 'r') do |file|
+  file.each_line do |line|
+    puts line.class
+    parsed_line = JSON.parse(line)
+
+    array_of_authors << {name: parsed_line['name'],
+                         username: parsed_line['username'],
+                         description: parsed_line['description'],
+                         followers_count: parsed_line['public_metrics']['followers_count'],
+                         following_count: parsed_line['public_metrics']['following_count'],
+                         tweet_count: parsed_line['public_metrics']['tweet_count'],
+                         listed_count: parsed_line['public_metrics']['listed_count']}
+
+    i += 1
+    if i == 5
+      lol = DATABASE[:authors].multi_insert(array_of_authors)
+      puts 'Endujem sa :)'
+      puts lol
+      exit
+    end
+  end
+end
+                    #=end
 
 puts "lol"
